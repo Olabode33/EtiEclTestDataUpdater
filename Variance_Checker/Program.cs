c121cc78d8564ee4cfae247feea2beb19bd62ba5
Danny <ekumdaniel@gmail.com>
2020-08-24 21:43:47 +0100

    Added variance checker

---
+++ Variance_Checker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variance_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VarianceChecker varianceChecker = new VarianceChecker();
+            varianceChecker.ExtractLGDFromExcel();
+        }
+    }
+}
